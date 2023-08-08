43824d8d22b40bec11dea92875f146ac5b6492bd
qnquangg <quang.ng193@gmail.com>
2023-08-08 17:33:58 +0700

    update trekking camera UI

--- Codebase/src/screens/TrekkingCamera/TrekkingCameraViewModel.ts
+++ Codebase/src/screens/TrekkingCamera/TrekkingCameraViewModel.ts
@@ -1,31 +1,70 @@
-import { useEffect, useState } from "react";
-import { AppState, AppStateStatus } from "react-native";
-import { Camera, Frame } from "react-native-vision-camera";
-declare let _WORKLET: true | undefined;
+import { useIsFocused } from "@react-navigation/native"
+import { useCallback, useEffect, useRef, useState } from "react"
+import { AppState, AppStateStatus } from "react-native"
+import { Camera, CameraRuntimeError, useCameraDevices } from "react-native-vision-camera"
+
 const CameraViewModel = () => {
-  const requestCameraPermissions = async () => {
-    const cameraPermission = await Camera.requestCameraPermission();
-    const microPermission = await Camera.requestMicrophonePermission()
-  }
-
-
   const useIsForeground = (): boolean => {
-    const [isForeground, setIsForeground] = useState(true);
+    const [isForeground, setIsForeground] = useState(true)
     useEffect(() => {
       const onChange = (state: AppStateStatus): void => {
-        setIsForeground(state === 'active');
-      };
-      const listener = AppState.addEventListener('change', onChange);
-      return () => listener.remove();
-    }, [setIsForeground]);
-    return isForeground;
-  };
+        setIsForeground(state === 'active')
+      }
+      const listener = AppState.addEventListener('change', onChange)
+      return () => listener.remove()
+    }, [setIsForeground])
+    return isForeground
+  }
+  
+  const cameraRef = useRef<Camera>(null)
+  const [hasMicrophonePermission, setHasMicrophonePermission] = useState(false)
+  // check if camera page is active
+  const isFocussed = useIsFocused()
+  const isForeground = useIsForeground()
+  const isActive = isFocussed && isForeground
+  // camera format settings
+  const devices = useCameraDevices()
+  const device = devices["back"]
 
+  const requestCameraPermissions = async () => {
+    // TODO: make it more stable
+    await Camera.requestCameraPermission()
+    await Camera.requestMicrophonePermission()
+  }
 
+  const onError = useCallback((error: CameraRuntimeError) => {
+    console.error(error)
+  }, [])
+
+  const onInitialized = useCallback(() => {
+    console.log("Camera initialized!")
+  }, [])
+
+  useEffect(() => {
+    Camera.getMicrophonePermissionStatus().then((status) =>
+      setHasMicrophonePermission(status === "authorized")
+    )
+  }, [])
+
+  const takePhoto = async () => {
+    if (cameraRef.current === null) {
+      console.log("cameraRef is null!")
+      return
+    }
+    const photo = await cameraRef.current.takePhoto()
+    // TODO: handle save photo
+    console.log({ photo })
+  }
 
   return {
+    cameraRef,
+    device,
+    isActive,
+    hasMicrophonePermission,
     requestCameraPermissions,
-    useIsForeground,
+    onError,
+    onInitialized,
+    takePhoto,
   }
 }
 export const useCameraViewModel = CameraViewModel
--- Codebase/src/screens/TrekkingCamera/style/trekkingCameraStyle.ts
+++ Codebase/src/screens/TrekkingCamera/style/trekkingCameraStyle.ts
@@ -2,7 +2,20 @@
 import { StyleSheet } from 'react-native'
 
 export const cameraStyle = StyleSheet.create({
-	container: {
-		flex: 1
-	},
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  captureButton: {
+    backgroundColor: 'white',
+    height: 80,
+    width: 80,
+    borderRadius: 80
+  },
+  text: {
+    color: "white",
+    fontSize: 11,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 })
