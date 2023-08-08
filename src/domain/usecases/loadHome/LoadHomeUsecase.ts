231febbc788c8c3f43d82c057a504ef164dd5924
dinh.truong@zien.vn <tqdinhhcmus@gmail.com>
2023-08-08 17:35:23 +0700

    remove unuseFile

--- src/domain/usecases/loadHome/LoadHomeUsecase.ts
+++
@@ -1,7 +0,0 @@
-export interface LoadHomeUsecase {} 
-export class LoadHomeUsecaseImpl implements LoadHomeUsecase {
-private repository: LoadHomeRepository
-constructor(_repository: LoadHomeRepository) {
-  this.repository = _repository
-}
-} 
