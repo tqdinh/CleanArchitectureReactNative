8a2204b4c848647729672467bcbbe05f0a360658
qnquangg <quang.ng193@gmail.com>
2023-08-10 22:02:32 +0700

    Implement UI: Trekking Map

--- Codebase/src/screens/TrekkingMap/TrekkingMap.tsx
+++ Codebase/src/screens/TrekkingMap/TrekkingMap.tsx
@@ -1,10 +1,35 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import Mapbox from '@rnmapbox/maps';
+import { trekkingMapStyle } from './style';
+import { Ionicons } from 'themes/appIcon'
+
+Mapbox.setAccessToken('pk.eyJ1IjoidHFkaW5oaGNtdXMiLCJhIjoiY2xsNG5teDZzMDZkcDNmb2dpcnljbGpzbyJ9.FIQMCyKAxOnFiYZCZ9wsHQ');
 
 const TrekkingMap = () => {
+
+  const [isPause, setIsPause] = useState(true)
+
   return (
-    <View>
-      <Text>TrekkingMap</Text>
+    <View style={{ flex: 1, justifyContent: 'space-between' }}>
+      <View style={trekkingMapStyle.container}>
+        <Mapbox.MapView style={trekkingMapStyle.map} />
+      </View>
+
+      <View style={{ flexDirection: 'row', marginBottom: 30, justifyContent: 'space-around' }}>
+        <TouchableOpacity onPress={() => { setIsPause(!isPause) }} style={trekkingMapStyle.button}>
+          <Text style={trekkingMapStyle.text}>{isPause? 'PAUSE' : 'RESUME'}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => { }} style={[trekkingMapStyle.button, {backgroundColor: '#0000CD'}]}>
+          <Ionicons name={'camera-outline'} size={20} style={{ alignSelf: 'center' }} />
+        </TouchableOpacity>
+
+
+        <TouchableOpacity onPress={() => { setIsPause(!isPause) }} style={[trekkingMapStyle.button, {opacity: isPause? 0 : 1}]}>
+          <Text style={trekkingMapStyle.text}>{'Finish'}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- Codebase/src/screens/TrekkingMap/style/trekkingMapStyle.ts
+++ Codebase/src/screens/TrekkingMap/style/trekkingMapStyle.ts
@@ -1,8 +1,30 @@
 
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
+
+const width = Dimensions.get("window").width
+const height = Dimensions.get("window").height
 
 export const trekkingMapStyle = StyleSheet.create({
   container: {
-    flex: 1
+    height: height*0.7,
+    width: width,
+  },
+  map: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  button: {
+    height: 80,
+    width: 80,
+    borderRadius: 80 / 2,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#E1E1E1',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 11,
+    alignSelf: 'center',
   },
 })
