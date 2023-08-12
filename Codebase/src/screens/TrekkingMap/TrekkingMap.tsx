1a3316db51b29c030b37f646deb4a999b2f3e67c
qnquangg <quang.ng193@gmail.com>
2023-08-12 16:10:29 +0700

    Implement show user current position

--- Codebase/src/screens/TrekkingMap/TrekkingMap.tsx
+++ Codebase/src/screens/TrekkingMap/TrekkingMap.tsx
@@ -1,33 +1,87 @@
-import { Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
-import Mapbox from '@rnmapbox/maps';
-import { trekkingMapStyle } from './style';
-import { Ionicons } from 'themes/appIcon'
-
-Mapbox.setAccessToken('pk.eyJ1IjoidHFkaW5oaGNtdXMiLCJhIjoiY2xsNG5teDZzMDZkcDNmb2dpcnljbGpzbyJ9.FIQMCyKAxOnFiYZCZ9wsHQ');
+import { Text, TouchableOpacity, View } from "react-native"
+import React, { useState } from "react"
+import MapboxGL, { UserLocationRenderMode } from "@rnmapbox/maps"
+import { trekkingMapStyle } from "./style"
+import { Ionicons, MaterialCommunityIcons } from "themes/appIcon"
+MapboxGL.setAccessToken(
+  "pk.eyJ1IjoidHFkaW5oaGNtdXMiLCJhIjoiY2xsNG5teDZzMDZkcDNmb2dpcnljbGpzbyJ9.FIQMCyKAxOnFiYZCZ9wsHQ"
+)
 
 const TrekkingMap = () => {
-
   const [isPause, setIsPause] = useState(true)
+  const [followUserLocation, setFollowUserLocation] = useState(true)
 
   return (
-    <View style={{ flex: 1, justifyContent: 'space-between' }}>
-      <View style={trekkingMapStyle.container}>
-        <Mapbox.MapView style={trekkingMapStyle.map} />
+    <View style={trekkingMapStyle.container}>
+      <View style={trekkingMapStyle.mapContainer}>
+        <MapboxGL.MapView
+          style={trekkingMapStyle.map}
+          tintColor={"red"}
+          logoEnabled={false}
+        >
+          <MapboxGL.Camera
+            followUserLocation={followUserLocation}
+            followZoomLevel={18}
+            onUserTrackingModeChange={(event) => {
+              if (!event.nativeEvent.payload.followUserLocation) {
+                setFollowUserLocation(false)
+              }
+            }}
+          />
+          <MapboxGL.UserLocation
+            visible={true}
+            renderMode={UserLocationRenderMode.Native}
+            showsUserHeadingIndicator={true}
+            androidRenderMode={"normal"}
+          />
+        </MapboxGL.MapView>
+        <View style={trekkingMapStyle.buttonCurrentPosition}>
+          <TouchableOpacity
+            onPress={() => setFollowUserLocation((prevState) => !prevState)}
+          >
+            <MaterialCommunityIcons
+              name={
+                followUserLocation
+                  ? "map-marker-radius"
+                  : "map-marker-radius-outline"
+              }
+              size={30}
+              style={{ alignSelf: "center" }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <View style={{ flexDirection: 'row', marginBottom: 30, justifyContent: 'space-around' }}>
-        <TouchableOpacity onPress={() => { setIsPause(!isPause) }} style={trekkingMapStyle.button}>
-          <Text style={trekkingMapStyle.text}>{isPause? 'PAUSE' : 'RESUME'}</Text>
+      <View style={trekkingMapStyle.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setIsPause(!isPause)
+          }}
+          style={trekkingMapStyle.button}
+        >
+          <Text style={trekkingMapStyle.text}>
+            {isPause ? "PAUSE" : "RESUME"}
+          </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => { }} style={[trekkingMapStyle.button, {backgroundColor: '#0000CD'}]}>
-          <Ionicons name={'camera-outline'} size={20} style={{ alignSelf: 'center' }} />
+        <TouchableOpacity
+          onPress={() => { }}
+          style={[trekkingMapStyle.button, { backgroundColor: "#D4F1F4" }]}
+        >
+          <Ionicons
+            name={"camera-outline"}
+            size={40}
+            style={{ alignSelf: "center" }}
+          />
         </TouchableOpacity>
 
-
-        <TouchableOpacity onPress={() => { setIsPause(!isPause) }} style={[trekkingMapStyle.button, {opacity: isPause? 0 : 1}]}>
-          <Text style={trekkingMapStyle.text}>{'Finish'}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setIsPause(!isPause)
+          }}
+          style={[trekkingMapStyle.button, { opacity: isPause ? 0 : 1 }]}
+        >
+          <Text style={trekkingMapStyle.text}>{"Finish"}</Text>
         </TouchableOpacity>
       </View>
     </View>
--- Codebase/src/screens/TrekkingMap/style/trekkingMapStyle.ts
+++ Codebase/src/screens/TrekkingMap/style/trekkingMapStyle.ts
@@ -1,30 +1,49 @@
-
-import { Dimensions, StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from "react-native"
 
 const width = Dimensions.get("window").width
 const height = Dimensions.get("window").height
 
 export const trekkingMapStyle = StyleSheet.create({
   container: {
-    height: height*0.7,
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  mapContainer: {
+    height: height * 0.7,
     width: width,
   },
   map: {
     flex: 1,
     borderWidth: 2,
-    borderColor: 'black',
+    borderColor: "black",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginBottom: 30,
+    justifyContent: "space-around",
   },
   button: {
     height: 80,
     width: 80,
     borderRadius: 80 / 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 1,
-    borderColor: '#E1E1E1',
-    justifyContent: 'center',
+    borderColor: "#E1E1E1",
+    justifyContent: "center",
   },
   text: {
     fontSize: 11,
-    alignSelf: 'center',
+    alignSelf: "center",
+  },
+  buttonCurrentPosition: {
+    position: "absolute",
+    height: 40,
+    width: 40,
+    borderRadius: 5,
+    backgroundColor: "white",
+    opacity: 0.7,
+    marginTop: 30,
+    right: 10,
+    justifyContent: "center",
   },
 })
