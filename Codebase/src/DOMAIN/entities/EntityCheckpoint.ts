c6afeb6f7388de855bf35d4596f75bfc97bf697a
qnquangg <quang.ng193@gmail.com>
2023-08-28 23:35:02 +0700

    Remove unnecessary id in Entity

--- Codebase/src/DOMAIN/entities/EntityCheckpoint.ts
+++ Codebase/src/DOMAIN/entities/EntityCheckpoint.ts
@@ -1,18 +1,15 @@
 export default class EntityCheckpoint {
-  private id: number
   private journey_id: number
   private description: string
   private starting_time: Date
   private date_created: Date
 
   constructor(
-    _id: number,
     _journey_id: number,
     _description: string,
     _starting_time: Date,
     _date_created: Date
   ) {
-    this.id = _id
     this.journey_id = _journey_id
     this.description = _description
     this.starting_time = _starting_time
--- Codebase/src/DOMAIN/entities/EntityPhoto.ts
+++ Codebase/src/DOMAIN/entities/EntityPhoto.ts
@@ -1,5 +1,4 @@
 export default class EntityPhoto {
-  // private id: number
   private checkpoint_id: number
   private photo_url: string
   private name: string
@@ -7,14 +6,12 @@ export default class EntityPhoto {
   private date_created: number
 
   constructor(
-    // _id: number,
     _checkpoint_id: number,
     _photo_url: string,
     _name: string,
     _coordinates: [number, number],
     _date_created: number
   ) {
-    // this.id = _id
     this.checkpoint_id = _checkpoint_id
     this.photo_url = _photo_url
     this.name = _name
